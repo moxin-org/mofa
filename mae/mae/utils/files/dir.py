62ca4e874486cc13f91dd9695b0ac84949209b13
ChenZi <chencheng0103@outlook.com>
2024-07-26 17:27:05 +0800

    Feat: 1. Creating a dynamic splicing process for the agent.

--- mae/mae/utils/files/dir.py
+++ mae/mae/utils/files/dir.py
@@ -2,6 +2,8 @@ import shutil
 from pathlib import Path
 from typing import Union
 import os
+from typing import Optional
+
 
 def check_dir(dir_path: Union[Path, str]):
     # Convert the input to a Path object if it's not already
@@ -20,8 +22,7 @@ def remove_dir(dir_path: Union[str, Path]):
         shutil.rmtree(dir_path)
 
 
-
-def delete_all_files_in_folder(folder_path:str):
+def delete_all_files_in_folder(folder_path: str):
     """
     Delete all files and subfolders in the specified folder.
 
@@ -39,3 +40,26 @@ def delete_all_files_in_folder(folder_path:str):
             print(f'Failed to delete {file_path}. Reason: {e}')
 
 
+def get_relative_path(current_file: str, sibling_directory_name: Optional[str] = 'sibling_directory',
+                      target_file_name: Optional[str] = 'target_file') -> str:
+    """
+    Get the relative path to the specified file in a sibling directory.
+
+    Parameters:
+    current_file (str): Path to the current script file.
+    sibling_directory_name (str, optional): Name of the sibling directory, default is 'sibling_directory'.
+    target_file_name (str, optional): Name of the target file, default is 'target_file'.
+
+    Returns:
+    str: Relative path to the target file.
+    """
+    # Directory of the current script
+    current_dir = os.path.dirname(current_file)
+
+    # Parent directory of the current directory
+    parent_dir = os.path.dirname(current_dir)
+
+    # Relative path to the target file in the sibling directory
+    target_file_relative_path = os.path.join(parent_dir, sibling_directory_name, target_file_name)
+
+    return target_file_relative_path
