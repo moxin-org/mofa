fdf09138d7f3b700588f76c3c95be81d12db83a7
ChenZi <chencheng0103@outlook.com>
2024-08-17 10:43:50 +0800

    Feat: 1. Creating a new `agent-hub` module, continuously adding content. 2. Updating the `server` module, modifying some API ports.

--- mae/mae/cli.py
+++ mae/mae/cli.py
@@ -4,20 +4,21 @@ import yaml
 from pathlib import Path
 from pathlib import Path
 
+from mae import examples_dir_path
+from mae.agent_link.agent_template import agent_template_path
 from mae.utils.files.dir import get_subdirectories
 
-current_path = Path.cwd()
 from mae.agent_link.merge_agents.merge_dataflow import MergeDataflow
 
 @click.command()
 @click.option(
-    '--main-dataflow-yml',
+    '--upstream-dataflow-yml',
     required=True,
     type=click.Path(exists=True),
     help="Main dataflow YAML file path. Example: --main-dataflow-yml ./Moxin-App-Engine/mae/mae/agent_link/agent_template/web_search/web_search_dataflow.yml"
 )
 @click.option(
-    '--additional-dataflow-yml',
+    '--downstream-dataflow-yml',
     required=True,
     type=click.Path(exists=True),
     help="Additional dataflow YAML file path. Example: --additional-dataflow-yml ./Moxin-App-Engine/mae/mae/agent_link/agent_template/reasoner/reasoner_dataflow.yml"
@@ -31,26 +32,27 @@ from mae.agent_link.merge_agents.merge_dataflow import MergeDataflow
     '--output-directory',
     type=click.Path(exists=True),
     help="Output directory path. Example: --output-directory /path/to/output_directory",
-    default = str(current_path.parent /'examples'/'generate')
+    default = str(examples_dir_path) + '/generate'
 )
 @click.option(
     '--search-directory',
     type=click.Path(exists=True),
     help="Search directory path. Example: --search-directory ./Moxin-App-Engine/mae/mae/agent_link/agent_template",
-    default=str(current_path/'agent_link'/'agent_template')
+    default=str(agent_template_path)
 )
-def generate_agents_cli(main_dataflow_yml:str, additional_dataflow_yml:str, dependencies:str, output_directory:str, search_directory:str):
+def agent_link_cli(upstream_dataflow_yml:str, downstream_dataflow_yml:str, dependencies:str, output_directory:str, search_directory:str):
     """
     CLI to generate merged agents configuration.
     """
     dependencies = json.loads(dependencies)
 
     merge_dataflow = MergeDataflow()
-    merge_dataflow.copy_agents_files(agents_dir_path=search_directory, agents_name=get_subdirectories(directory=search_directory),
+
+    merge_dataflow.copy_agents_files(agents_dir_path=search_directory, agents_name=[i for i in get_subdirectories(directory=search_directory) if i+'_dataflow' in [str(Path(upstream_dataflow_yml).name.replace('.yml', '').replace('.yaml', '')), str(Path(downstream_dataflow_yml).name.replace('.yml', '').replace('.yaml', ''))]],
                                      output_dir_path=output_directory, subdirectories=['scripts', 'configs'])
     merged_agents = merge_dataflow.generate_agents(
-        main_dataflow_yml=main_dataflow_yml,
-        additional_dataflow_yml=additional_dataflow_yml,
+        main_dataflow_yml=upstream_dataflow_yml,
+        additional_dataflow_yml=downstream_dataflow_yml,
         dependencies=[dependencies],
         output_dir_path=output_directory,
         search_directory=search_directory
@@ -66,5 +68,5 @@ if __name__ == '__main__':
     # search_directory = str(current_path/'agent_link'/'agent_template')
     # generate_agents_cli(main_dataflow_yml=main_dataflow_yml, additional_dataflow_yml=additional_dataflow_yml,
     #                     dependencies=dependencies, output_directory=output_directory, search_directory=search_directory)
-    generate_agents_cli()
+    agent_link_cli()
 
