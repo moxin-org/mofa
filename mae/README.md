a7027596c2d4dd9998899c6a1cfb549111cf09db
ChenZi <chencheng0103@outlook.com>
2024-09-30 17:42:39 +0800

    feat: remove mae

--- mae/README.md
+++
@@ -1,6 +0,0 @@
-# MAE---Moxin-App-Engine
-MAE - Moxin App Engine that powers agentic applications
-
-
-[README.html](docs%2FMAE%2Fbuild%2Fhtml%2Findex.html)
-
--- mae/mae/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-from pathlib import Path
-
-# 获取当前文件（即 __init__.py）的绝对路径
-package_root = os.path.abspath(os.path.dirname(__file__))
-mae_path = os.path.join(package_root,  )
-examples_dir_path = Path(str(mae_path)).parent / 'examples'
-cli_dir_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), )
-agent_dir_path = str(Path(str(cli_dir_path)).parent / 'examples')
