1579c5fbf23fb5762636dee69aa54c662535a76e
ChenZi <chencheng0103@outlook.com>
2024-11-13 11:00:10 +0800

    feat: update mofa search dataflow

--- python/examples/search/scripts/search_box_connector_agent.py
+++ python/examples/search/scripts/search_box_connector_agent.py
@@ -2,10 +2,7 @@ import json
 import os
 from dora import Node, DoraStatus
 import pyarrow as pa
-import asyncio
-
-from agent_core.connector_core import load_url_with_crawl4ai, load_url_with_selenium
-
+from agent_core.connector_core import  load_search_box
 from mofa.kernel.utils.util import load_agent_config, create_agent_output
 from mofa.utils.files.dir import get_relative_path
 
@@ -19,15 +16,28 @@ class Operator:
         if dora_event["type"] == "INPUT" and dora_event['id'] == "search_text":
             search_text = dora_event["value"][0].as_py()
             yaml_file_path = get_relative_path(current_file=__file__, sibling_directory_name='configs',
-                                               target_file_name='discovery_search_box_agent.yml')
-            configs = load_agent_config(yaml_file=yaml_file_path, is_expand=True)
-            file_data = ''
+                                               target_file_name='search_box_connector_agent.yml')
+            configs = load_agent_config(yaml_file=yaml_file_path)
+            json_data = ''
             with open(configs.get('file_path'), 'r', encoding='utf-8') as f:
                 # 读取JSON文件内容并解析为Python字典
-                file_data = json.load(f)
-
-            result = {'html_content':html_content,'url':url}
-            print(result)
-            send_output("load_url_connector_response", pa.array([create_agent_output(step_name='load_url_connector_response', output_data=result, dataflow_status=os.getenv('IS_DATAFLOW_END', False))]), dora_event['metadata'])
-
+                json_data = json.load(f)
+            max_iteration = configs.get('max_iteration',3)
+            for num in range(0,max_iteration):
+                search_box_html_datas = load_search_box(url=json_data['url'], search_box_html_result=json_data['search_box_list'],search_text=search_text)
+                if len(search_box_html_datas)>0:
+                    break
+            send_output("search_box_connector_response", pa.array([create_agent_output(step_name='search_box_connector_response', output_data=search_box_html_datas, dataflow_status=os.getenv('IS_DATAFLOW_END', False))]), dora_event['metadata'])
+            print('-------  , ',search_box_html_datas)
         return DoraStatus.CONTINUE
+
+
+
+
+# search_text = 'opea'
+# yaml_file_path = '/Users/chenzi/project/zcbc/mofa/python/examples/search/data/output/discovery_search_box_agent_response.json'
+# json_data = load_agent_config(yaml_file=yaml_file_path)
+#
+# search_box_html_datas = load_search_box(url=json_data['url'], search_box_html_result=json_data['search_box_list'],search_text=search_text)
+# print(search_box_html_datas)
+
