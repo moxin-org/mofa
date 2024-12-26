bf1f5c063bab616cb7606dcb4c9ebf4b56346dc1
ChenZi <chencheng0103@outlook.com>
2024-12-26 22:14:03 +0800

    1. Integrate the `memory_retrieval` node, establish the corresponding structure, and utilize `mem0` as the package for memory operations. 2. Modify parts of the code within the `agent-template`. 3. Test the combined `terminal-input` and `memory-retrieval` to verify the entire workflow. 4. Enhance the functionality and code of the `BaseMofaAgent`.

---
+++ python/agent-hub/memory-retrieval/memory_retrieval/main.py
@@ -0,0 +1,94 @@
+import argparse
+import json
+import os
+from mem0 import Memory
+
+from typing import Any, Dict
+from dora import Node
+from mofa.agent_build.base.base_agent import BaseMofaAgent
+from mofa.kernel.utils.util import create_agent_output
+from mofa.utils.files.read import read_yaml
+import pyarrow as pa
+RUNNER_CI = True if os.getenv("CI") == "true" else False
+
+class MemoryRetrievalAgent(BaseMofaAgent):
+    
+    
+    def load_config(self, config_path: str = None) -> Dict[str, Any]:
+        if config_path is None:
+            config_path = self.config_path
+        return read_yaml(file_path=config_path)['agent']['llm']
+    def load_user_id(self,config_path:str=None):
+        if config_path is None:
+            config_path = self.config_path
+        return read_yaml(file_path=config_path)['agent']['user_id']
+    def create_llm_client(self,config:dict=None,*args,**kwargs):
+        self.init_llm_config()
+        os.environ["OPENAI_API_KEY"] = os.environ.get('LLM_API_KEY')
+        if os.environ.get('LLM_API_URL',None) is not None:
+            os.environ["OPENAI_API_BASE"] = os.environ.get('LLM_API_URL')
+        mem0_config = self.load_config()
+        m = Memory.from_config(mem0_config)
+        self.llm_client = m
+        return self
+    def get_mem0_search_text(self,memory_datas):
+        try:
+            results = list(set([memory_data.get("memory") for memory_data in memory_datas]))
+        except Exception as e :
+            results = memory_datas.get("results")
+        return results
+    def run(self, task:str=None,*args, **kwargs):
+        mem0_config = self.load_config()
+        self.create_llm_client(mem0_config)
+        user_id = self.load_user_id()
+        memory_result = self.llm_client.search(task, user_id=user_id)
+        results = self.get_mem0_search_text(memory_datas=memory_result)
+        return results
+
+def main():
+
+    # Handle dynamic nodes, ask for the name of the node in the dataflow, and the same values as the ENV variables.
+    parser = argparse.ArgumentParser(description="Mem0 Agent")
+
+    parser.add_argument(
+        "--name",
+        type=str,
+        required=False,
+        help="The name of the node in the dataflow.",
+        default="arrow-assert",
+    )
+    parser.add_argument(
+        "--task",
+        type=str,
+        required=False,
+        help="Tasks required for the memmory agent.",
+        default="Paris Olympics",
+    )
+
+    args = parser.parse_args()
+    task = os.getenv("TASK", args.task)
+
+    node = Node(
+        args.name
+    )  # provide the name to connect to the dataflow if dynamic node
+
+    # assert_data = ast.literal_eval(data)
+    for event in node:
+ 
+        if event["type"] == "INPUT" and event['id'] in ['task','data']:
+            task = event["value"][0].as_py()
+            memmory = MemoryRetrievalAgent(config_path=os.path.join(os.path.abspath(os.path.dirname(__file__)), ) + '/configs/config.yml',
+                                llm_config_path=os.path.join(os.path.abspath(os.path.dirname(__file__)), ) + '/.env.secret')
+            result = memmory.run(task=task)
+            # memmory.send_output(node=node,output_name='memeory_retrieval_result',output_data=result,is_end_dataflow=os.getenv('IS_DATAFLOW_END',True),
+            #                     event=event,)
+            print('memeory_retrieval_result : ',result)
+            output_name = 'memeory_retrieval_result'
+            node.send_output(output_name, pa.array([create_agent_output(step_name=output_name, output_data=result,dataflow_status=os.getenv('IS_DATAFLOW_END',True))]), event['metadata'])
+        event = node.next(timeout=200)
+if __name__ == "__main__":
+    main()
+    # mem0 = MemoryRetrievalAgent(config_path=os.path.join(os.path.abspath(os.path.dirname(__file__)), ) + '/configs/config.yml',
+    #                             llm_config_path=os.path.join(os.path.abspath(os.path.dirname(__file__)), ) + '/.env.secret')
+    # result = mem0.run(task='How to use the mem0-ai package in Python? ')
+    # print(result)
--- python/mofa/agent_build/base/base_agent.py
+++ python/mofa/agent_build/base/base_agent.py
@@ -2,24 +2,26 @@ import os
 import attrs
 import pyarrow as pa
 from attrs import define, field
-from abc import ABC, abstractmethod
 from typing import Any, Dict, Union
 from mofa.kernel.utils.util import create_agent_output, load_node_result
 from mofa.utils.files.read import read_yaml
 import yaml
 from dora import Node
+from dotenv import load_dotenv
+from pathlib import Path
 
 @define
-class BaseMofaAgent(ABC):
+class BaseMofaAgent():
     """
     A base class for MOFA agents, providing configuration management,
     resource initialization, and a context manager interface.
     """
 
     config_path: Union[str, None] = field(default=None)
-    config: Dict[str, Any] = field(init=False,factory={})
+    config: Dict[str, Any] = field(init=False,factory=dict)
     llm_client: Any = field(init=False, default=None)
-
+    llm_config_path: Union[str, None] = field(default='.env.secret')
+    
     def __attrs_post_init__(self) -> None:
         """
         Automatically called by attrs after the object is instantiated.
@@ -29,6 +31,11 @@ class BaseMofaAgent(ABC):
         if self.config_path is None:
             self.config_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), ) + '/configs/agent.yml'
         
+    def init_llm_config(self,llm_config_path:str=None):
+        if llm_config_path is None :
+            llm_config_path = self.llm_config_path
+        if Path(llm_config_path).exists():
+            load_dotenv(llm_config_path)
 
     def load_config(self, config_path: str=None) -> Dict[str, Any]:
         """
@@ -43,7 +50,7 @@ class BaseMofaAgent(ABC):
             output_step_name = output_name
         node.send_output(output_name, pa.array([create_agent_output(step_name=output_step_name, output_data=output_data,dataflow_status=os.getenv('IS_DATAFLOW_END',is_end_dataflow))]), event['metadata'])
     
-    def run(self,*args,**kwargs):
+    def run(self,task:str=None,*args,**kwargs):
         pass
     
     def parse_agent_parameters(self,agent_input:Any):
@@ -52,3 +59,10 @@ class BaseMofaAgent(ABC):
         except:
             agent_input = agent_input
         return agent_input
+    
+    def create_llm_client(self,config:dict=None,*args,**kwargs):
+        if config is None:
+            config = self.config
+        pass 
+        
+        
