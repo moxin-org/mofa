10f8711047681b037591b5cd97e710ad64f598c8
ChenZi <chencheng0103@outlook.com>
2025-02-14 10:24:36 +0800

    use  python decorator create agent

--- python/agent-hub/agent-template/agent/main.py
+++ python/agent-hub/agent-template/agent/main.py
@@ -1,27 +1,18 @@
-import argparse
-import json
-import os
-from pathlib import Path
-from dora import Node
-from mofa.agent_build.base.base_agent import MofaAgent
-from mofa.kernel.utils.util import create_agent_output
-import pyarrow as pa
-import os
- 
+from mofa.agent_build.base.base_agent import MofaAgent, run_agent
 
+@run_agent
+def run(agent:MofaAgent):
+    task = agent.receive_parameter('task')
+
+    # TODO: 在下面添加你的Agent代码,其中agent_inputs是你的Agent的需要输入的参数
+    # reasoner = ReasonerAgent(config_path=os.path.join(os.path.abspath(os.path.dirname(__file__)), ) + '/configs/config.yml',
+    #                     llm_config_path=os.path.join(os.path.abspath(os.path.dirname(__file__)), ) + '/.env.secret')
+    # result = reasoner.run(**agent.agent_inputs)
+    # print('reasoner_result : ',result)
+    agent_output_name = 'agent_result'
+    agent.send_output(agent_output_name=agent_output_name,agent_result=task)
 def main():
-
-    agent = MofaAgent(agent_name='mofa-agent')
-    while True:
-        agent.receive_parameter()
-        print('agent_inputs : ',agent.agent_inputs)
-        # TODO: 在下面添加你的Agent代码,其中agent_inputs是你的Agent的需要输入的参数
-        # reasoner = ReasonerAgent(config_path=os.path.join(os.path.abspath(os.path.dirname(__file__)), ) + '/configs/config.yml',
-        #                     llm_config_path=os.path.join(os.path.abspath(os.path.dirname(__file__)), ) + '/.env.secret')
-        # result = reasoner.run(**agent.agent_inputs)
-        # print('reasoner_result : ',result)
-        result = 'agent_result' #
-        agent_output_name = 'reasoner_result'
-        agent.send_output(agent_output_name=agent_output_name,agent_result=result)
+    agent = MofaAgent(agent_name='you-agent-name')
+    run(agent=agent)
 if __name__ == "__main__":
     main()
