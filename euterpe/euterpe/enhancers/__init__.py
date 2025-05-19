9b594c54d26a3b53a07d143cb3f5aa3dbfba0a6d
betasecond <nbdhc9039@gmail.com>
2025-05-19 10:34:04 +0800

    remoe redundant denpendcy

--- euterpe/euterpe/enhancers/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Enhancers package for the Euterpe library.
-
-This package contains modules that enhance text prompts using AI services.
-"""
-
-from euterpe.enhancers.dify_client import DifyClient, DifyEnhancerConfig, DifyProcessingError
-
-__all__ = ["DifyClient", "DifyEnhancerConfig", "DifyProcessingError"]
