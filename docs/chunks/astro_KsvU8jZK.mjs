086d8cafdd3065eb69dd3a66c79b78881f58f718
BH3GEI <scholar.liyao@gmail.com>
2025-05-22 03:42:19 +0900

    Fix: Update base path for GitHub Pages deployment

--- docs/chunks/astro_KsvU8jZK.mjs
+++
@@ -1,3 +0,0 @@
-import 'kleur/colors';
-import './astro/server_Ba4Od88w.mjs';
-import 'clsx';
--- docs/chunks/astrowind-template-in-depth_BsA5EYmo.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./astrowind-template-in-depth_DLnlr1nK.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
-
-export { defaultMod as default };
