1f6c1e600ddfb1e554ffc2143b2a572f5735c0de
BH3GEI <scholar.liyao@gmail.com>
2025-05-23 04:56:17 +0900

    add landing page

---
+++ mofa-website/src/content/config.ts
@@ -0,0 +1,25 @@
+import { z, defineCollection } from 'astro:content';
+
+const docsCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+  }),
+});
+
+const blogCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.date(),
+    author: z.string(),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
+export const collections = {
+  'docs': docsCollection,
+  'blog': blogCollection,
+}; 
