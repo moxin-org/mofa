aea1c3f01f65ad01d2c5918994c7b6cf4705d7ac
BH3GEI <scholar.liyao@gmail.com>
2025-05-23 05:10:37 +0900

    Configure GitHub Pages with docs deployment

--- mofa-website/astro.config.mjs
+++ mofa-website/astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://moxin-org.github.io',
-  base: '/mofa'
+  base: '/mofa',
+  outDir: '../docs'
 });
