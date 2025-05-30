4fab7c474a1bf670218774f5627abaafce1170c4
betasecond <nbdhc9039@gmail.com>
2025-05-30 15:35:06 +0800

    delete unused agent

--- python/agent-hub/image2video/euterpe/euterpe/vendors/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Vendor libraries incorporated directly into Euterpe.
-
-This package contains vendor libraries that have been integrated directly 
-into the Euterpe project to simplify dependency management.
-"""
-
-# Re-export beatoven_ai and klingdemo modules
-from . import beatoven_ai, klingdemo
--- python/agent-hub/image2video/euterpe/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Test initialization for the Euterpe test suite."""
-
-# This file is required to make the tests directory a proper Python package
-# It can later include test fixtures and other shared test resources
